2167cfd98fd86bfb92e98e39bdfa308c041b340a
joel-jcs <joeljcs@protonmail.com>
2024-09-02 17:55:12 -0400

    added sketchpad grid which can be changed by setting a new squaresize. initialized with 16 to meet project requirements.

---
+++ script.js
@@ -0,0 +1,16 @@
+const sketchpad = document.getElementById('sketch-container');
+
+
+const colorBtn = document.getElementById('color-btn');
+
+const squareSize = 16;
+const gridSize = squareSize * squareSize;
+
+for (let i = 0; i < gridSize; i++) {
+    sketchpad.innerHTML += '<div class="square"></div>';
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+    square.style.flexBasis = `calc( 100% / ${squareSize})`;    
+});
