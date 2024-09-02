8d751b1556a83a45eeb453b09be31a1ccf9735ce
joel-jcs <joeljcs@protonmail.com>
2024-09-02 18:25:28 -0400

    added simple grid size selection functionality

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const sketchpad = document.getElementById('sketch-container');
+const gridSizeBtn = document.getElementById('grid-size-btn');
 
+let squareSize = 16;
+let gridSize = squareSize * squareSize;
 
-const colorBtn = document.getElementById('color-btn');
+const setGrid = () => {
 
-const squareSize = 16;
-const gridSize = squareSize * squareSize;
-
-for (let i = 0; i < gridSize; i++) {
-    sketchpad.innerHTML += '<div class="square"></div>';
+    for (let i = 0; i < gridSize; i++) {
+        sketchpad.innerHTML += '<div class="square"></div>';
+    }
+    
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.flexBasis = `calc( 100% / ${squareSize})`;
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'black';
+        });
+    });
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.style.flexBasis = `calc( 100% / ${squareSize})`;
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'black';
-    })
+setGrid();
+
+gridSizeBtn.addEventListener('click', () => {
+    const selection = prompt("Select a new grid size by typing a number from 1 to 100:");
+    if (!selection || selection < 1 || selection > 100) {
+        alert("You must input a grid size from 1 to 100.");
+    } else {
+        sketchpad.innerHTML = ``;
+        squareSize = selection;
+        gridSize = squareSize * squareSize;
+        setGrid();
+    }
 });
