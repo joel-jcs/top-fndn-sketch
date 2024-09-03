55bd8cefa23f7c8f150fdd4bfc7d81f69173224d
joel-jcs <joeljcs@protonmail.com>
2024-09-03 00:19:29 -0400

    + changed method to pick grid size - from prompt to slider that updates automatically. + created proper draw function to handle drawing in different scenarios such as when grid is changed

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const sketchpad = document.getElementById('sketch-container');
-const gridSizeBtn = document.getElementById('grid-size-btn');
-const rainbowBtn = document.getElementById('rainbow-btn')
+const gridSizeSlider = document.getElementById('grid-size-slider');
+const gridSizeSpan = document.getElementById('grid-size-span');
+const rainbowBtn = document.getElementById('rainbow-btn');
+const eraseBtn = document.getElementById('erase-btn');
+const clearBtn = document.getElementById('clear-btn');
+
 
 let squares;
 let squareSize = 16;
 let gridSize = squareSize * squareSize;
 
-const createGrid = () => {
+const setGrid = () => {
     for (let i = 0; i < gridSize; i++) {
         sketchpad.innerHTML += '<div class="square"></div>';
     }
@@ -17,56 +21,58 @@ const createGrid = () => {
     });
 }
 
-createGrid();
-
+setGrid();
+    
 let RGBA = [0, 0, 0];
 let pixelColor = ``;
 let isMouseDown = false;
 let rainbowMode = false;
 
-squares.forEach(square => {
-    let opacity = 0.1;
-
-    square.addEventListener('mousedown', () => {
-        isMouseDown = true;
-    });
-
-    square.addEventListener('mouseup', event => {
-        isMouseDown = false;
-    });
-
-    square.addEventListener('mouseenter', event => {
-        if (isMouseDown) {
-            pixelColor = `rgba(0,0,0,${opacity})`;
-            
-            let RGBA_COPY = RGBA.map(index => index = Math.floor(Math.random() * 256));
-            if (rainbowMode) {
-                RGBA = RGBA_COPY.slice();
-                pixelColor = `rgba(${RGBA[0]},${RGBA[1]},${RGBA[2]}, ${opacity})`;
-            } else {
+const draw = () => {
+    squares.forEach(square => {
+        let opacity = 0.1;
+    
+        square.addEventListener('mousedown', () => {
+            isMouseDown = true;
+        });
+    
+        square.addEventListener('mouseup', event => {
+            isMouseDown = false;
+        });
+    
+        square.addEventListener('mouseenter', event => {
+            if (isMouseDown) {
                 pixelColor = `rgba(0,0,0,${opacity})`;
+                
+                let RGBA_COPY = RGBA.map(index => index = Math.floor(Math.random() * 256));
+                if (rainbowMode) {
+                    RGBA = RGBA_COPY.slice();
+                    pixelColor = `rgba(${RGBA[0]},${RGBA[1]},${RGBA[2]}, ${opacity})`;
+                } else {
+                    pixelColor = `rgba(0,0,0,${opacity})`;
+                }
+    
+                square.style.backgroundColor = `${pixelColor}`;
+                opacity = Math.min(Number((opacity + 0.1).toFixed(1)), 1);
+    
+                console.log(pixelColor);
             }
+        })
+    });
+}
 
-            square.style.backgroundColor = `${pixelColor}`;
-            opacity = Math.min(Number((opacity + 0.1).toFixed(1)), 1);
-        }
-    })
-});
+draw();
 
-
-gridSizeBtn.addEventListener('click', () => {
-    const selection = prompt("Select a new grid size by typing a number from 1 to 100:");
-    if (!selection || selection < 1 || selection > 100) {
-        alert("You must input a grid size from 1 to 100.");
-    } else {
-        sketchpad.innerHTML = ``;
-        squareSize = selection;
-        gridSize = squareSize * squareSize;
-        setGrid();
-    }
+gridSizeSlider.addEventListener('input', () => {
+    selection = gridSizeSlider.value;
+    gridSizeSpan.textContent = `${selection} x ${selection}`
+    sketchpad.innerHTML = ``;
+    squareSize = selection;
+    gridSize = squareSize * squareSize;
+    setGrid();
+    draw();
 });
 
 rainbowBtn.addEventListener('click', () => {
     rainbowMode = !rainbowMode;
-    console.log(rainbowMode);
 });
